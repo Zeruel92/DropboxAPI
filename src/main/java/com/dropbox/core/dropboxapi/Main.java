747537898fbf6f04913f393728d4fab5655f410e
pspgt <gabry.t92@gmail.com>
2016-03-24 10:17:45 +0100

    Dropbox API example, upload file to root directory

---
+++ src/main/java/com/dropbox/core/dropboxapi/Main.java
@@ -0,0 +1,31 @@
+import com.dropbox.core.*;
+import com.dropbox.core.v2.*;
+import com.dropbox.core.v2.DbxFiles.Metadata;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.List;
+
+class Main {
+    static final String ACCESS_TOKEN = "Mk6Tr-1v8RYAAAAAAAB4H2hhwZlsZVVQn0X98wi8i2Zh5afXjSrxgmR2yc_J-ZFm";
+
+    public static void main(String args[]) throws DbxUsers.GetCurrentAccountException, DbxException, IOException {
+        // Create Dropbox client
+        DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+
+        // Get current account info
+        DbxUsers.FullAccount account = client.users.getCurrentAccount();
+        System.out.println(account.name.displayName);
+
+        // Get files and folder metadata from Dropbox root directory
+        List<Metadata> entries = client.files.listFolder("").entries;
+        
+        for (DbxFiles.Metadata metadata : entries) {
+            System.out.println(metadata.name);
+        }
+
+       // Upload "test.txt" to Dropbox
+        InputStream in = new FileInputStream("test.txt");
+        DbxFiles.FileMetadata metadata = client.files.uploadBuilder("/test.txt").run(in);
+    }
+}
